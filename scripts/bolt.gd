b35a81f1fd9001f820fec942cb536758f5ff71b1
Akshit Sharma <akshitsharma196@gmail.com>
2025-10-19 19:25:18 -0400

    added eemy, made them shoot bullets, randomised shooting time

--- scripts/bolt.gd
+++ scripts/bolt.gd
@@ -2,8 +2,10 @@ extends Node2D
 
 @export var speed: float = 400
 
+func  _ready():
+	await get_tree().create_timer(2.0).timeout
+	queue_free()
+
 func _process(delta):
 	position.y -= speed * delta
 	
-	if position.y<0:
-		queue_free()
--- scripts/enemy_1.gd
+++ scripts/enemy_1.gd
@@ -1,1 +1,29 @@
 extends CharacterBody2D
+
+@export var speed: float = 200
+var movementVector: Vector2 = Vector2(1,0)
+
+@onready var crossed = preload("res://scenes/crossed.tscn")
+
+var timerToSHoot: float
+
+func _physics_process(delta):
+	if position.x >= 1100:
+		movementVector.x = -1
+	elif position.x <= 52:
+		movementVector.x = 1
+	velocity = movementVector * speed
+	move_and_slide()
+	
+	if timerToSHoot > 2:
+		timerToSHoot = 0
+		var rng = randi_range(0,3)
+		if rng == 0:
+			var crossedCopy = crossed.instantiate()
+			crossedCopy.position = position
+			crossedCopy.position.y += 50
+			get_tree().get_root().add_child(crossedCopy)
+	else:
+		timerToSHoot+=delta
+	
+	
