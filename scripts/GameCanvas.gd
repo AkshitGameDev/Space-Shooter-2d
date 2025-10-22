7b616999156e80ce3def62cb0cf0f6d51da883d8
Akshit Sharma <akshitsharma196@gmail.com>
2025-10-22 18:22:19 -0400

    adding ui button functionality

---
+++ scripts/GameCanvas.gd
@@ -0,0 +1,26 @@
+extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_try_again_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/1.tscn")
+
+
+func _on_main_menu_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/MainMenu.tscn")
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/1.tscn")
+
+
+func _on_exit_button_pressed():
+	pass # Replace with function body.
